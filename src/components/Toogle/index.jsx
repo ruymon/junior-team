32a46eb0070470839e788d4cb6e8577bae7f765f
ruymon <ruystfrancis@gmail.com>
2021-06-07 18:36:34 -0300

    Add SEO Tags & Improve Accessibility

--- src/components/Toogle/index.jsx
+++ src/components/Toogle/index.jsx
@@ -5,6 +5,7 @@ import { Container } from "./styles";
 function Toogle({ children }) {
   return (
     <Container>
+      <label for="checkbox" className="sr-only">Checkbox Toogle</label>
       <input type="checkbox" id="checkbox" name="Toogle Checkbox"/>
       <span>{children}</span>
     </Container>
